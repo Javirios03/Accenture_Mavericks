4335364827efc55f663deb60b9fdf525d10ef3f2
GabriLazovsky <gabriel.lazovskyigual@usp.ceu.es>
2025-11-14 17:42:52 +0100

    models y server js

---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+// Clientes
+const clienteSchema = new mongoose.Schema({
+    nombre: String,
+    identificacion: String,
+    correo: String,
+    fechaRegistro: { type: Date, default: Date.now },
+    password: String // para login
+});
+
+// Cuentas
+const cuentaSchema = new mongoose.Schema({
+    idCliente: { type: mongoose.Schema.Types.ObjectId, ref: "Cliente" },
+    tipo: String, // prestamo, credito, inversion
+    saldoActual: Number,
+    fechaApertura: { type: Date, default: Date.now }
+});
+
+// Movimientos
+const movimientoSchema = new mongoose.Schema({
+    idCuenta: String, //{ type: mongoose.Schema.Types.ObjectId, ref: "Cuenta" },
+    tipo: String, // abono, cargo, interes
+    monto: Number,
+    fecha: { type: Date, default: Date.now },
+    descripcion: String
+});
+
+const Cliente = mongoose.model("Cliente", clienteSchema);
+const Cuenta = mongoose.model("Cuenta", cuentaSchema);
+const Movimiento = mongoose.model("Movimiento", movimientoSchema);
+
+module.exports = { Cliente, Cuenta, Movimiento };
